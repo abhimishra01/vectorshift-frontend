52e325fc7026da39b45b5fbe31d73a1f41d9b978
Abhishek Mishra <abhisk.mhra@gmail.com>
2024-11-16 22:22:52 +0530

    chore: remove unwanted logs from api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,16 +2,12 @@ import axios from "axios";
 
 export const parsePipeline = async (payload) => {
   try {
-    console.log("PAYLOAD");
-    console.log(payload);
     const response = await axios.post(
       "http://127.0.0.1:8000/pipelines/parse",
       payload
     );
-    console.log(response);
     return response.data;
   } catch (error) {
-    console.error(error);
     return error.message;
   }
 };
