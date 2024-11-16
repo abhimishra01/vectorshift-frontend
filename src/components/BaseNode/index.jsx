9f0543085bbea2f837d8420cc03d586c58e5b9a9
Abhishek Mishra <abhisk.mhra@gmail.com>
2024-11-16 08:26:27 +0530

    wip: BaseNode component

--- src/components/BaseNode/index.jsx
+++ src/components/BaseNode/index.jsx
@@ -1,5 +1,13 @@
 // BaseNode | Abstraction of Nodes
 
-const BaseNode = ({ id, data, handlers }) => {};
+const BaseNode = ({ id, data, name, handlers }) => {
+  return (
+    <div style={{ width: 200, height: 80, border: "1px solid black" }}>
+      <div>
+        <span>{name}</span>
+      </div>
+    </div>
+  );
+};
 
 export default BaseNode;
