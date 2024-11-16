de1460aae626c011c8c8760f02a41bb7c12de4e7
Abhishek Mishra <abhisk.mhra@gmail.com>
2024-11-16 12:33:28 +0530

    style: remove linear gradient background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { SubmitButton } from "./submit";
 
 function App() {
   return (
-    <div className="gradient">
+    <div>
       <PipelineToolbar />
       <PipelineUI />
       <SubmitButton />
